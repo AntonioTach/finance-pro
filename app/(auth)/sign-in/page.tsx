6146356e1d15e1219a1761b885ee5477ff0376f8
AntonioTach <antach.vihe@gmail.com>
2024-11-07 23:48:01 -0600

    FEAT: Update sign-in form to use authTypes and comment out lint job in CI workflow

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,10 +1,11 @@
 import AuthForm from '@/components/AuthForm';
+import { authTypes } from '@/lib/utils';
 import React from 'react';
 
 const SignIn = () => {
   return (
     <section className="flex-center size-full max-sm:px-6">
-      <AuthForm type="sign-in" />
+      <AuthForm type={authTypes.SIGN_IN} />
     </section>
   );
 };
